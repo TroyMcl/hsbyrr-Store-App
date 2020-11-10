74e849e98fc93a1ceceaa1b9ac9cee4037e408af
Troy <troyjmcl85@gmail.com>
2020-11-10 10:43:45 -0700

    Added some initial styling for header componet with material-ui to go with theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton'
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  font: {
+    color: '#ffffff',
+    textDecoration: 'none'
+  },
+
+  appBar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+})
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <div className="ui secondary menu">
-      <Link to="/" className="item">
-        Store
-      </Link>
-      <div>
-        <Link to="/cart" className="item">
-          Shopping Cart
+    <AppBar position="static" style={{marginBottom: '20px'}}>
+      <Toolbar className={classes.appBar}>
+        <Link to="/" className="item">
+        <Button className={classes.font}>Store</Button>
         </Link>
-      </div>
-    </div>
+        <Link to="/cart" className="item">
+          <IconButton edge="start" className={classes.font}>
+            <ShoppingCartIcon />
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
   )
 };
 
+{/* <Toolbar className="classes.headerStyles">
+  <Grid container>
+    <Link className="classes.font" to="/" className="item">
+      Store
+    </Link>
+    <Grid item md={10} />
+    <Link to="/cart" className="item">
+      <ShoppingCartIcon />
+    </Link>
+  </Grid>
+</Toolbar> */}
 export default Header;
