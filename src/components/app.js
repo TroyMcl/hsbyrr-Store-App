a30e36dd05477b6efcdabfeeae0a438f033b30b5
Troy <troyjmcl85@gmail.com>
2020-11-10 10:50:13 -0700

    Added cusomized themefor material ui and wrapped component in material-ui ThemeProvider removed semantic-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { ThemeProvider } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core';
 
+import theme from '../theme';
 import StoreList from './products/StoreList';
 import Header from './Header';
 import ItemView from './products/ItemView';
@@ -13,46 +16,55 @@ const App = (props) => {
 
   const addToCart = (e, item) => {
     console.log(e, item);
-    const updatedShoppingCart = [...shoppingCart, item];
-    useShoppingCart(updatedShoppingCart);
+    const index = shoppingCart.findIndex((itemObj => itemObj.item === item));
+    if (index === -1) {
+      const updatedShoppingCart = [...shoppingCart, {item, qty: 1}];
+      useShoppingCart(updatedShoppingCart);
+    } else {
+      shoppingCart[index].qty++;
+      useShoppingCart([...shoppingCart]);
+    }
+
   };
 
   return (
-    <Grid container direction="column">
-      <BrowserRouter>
-        <Grid item>
-          <Header />
-        </Grid>
-        <Grid item container>
-          <Grid item xs={1} sm={2} />
-            <Grid item xs={12} sm={8}>
-            <Switch>
-              <Route
-                path="/"
-                exact
-                render={(props) => <StoreList {...props} addToCart={addToCart}/>}
-              />
-              <Route
-                path="/product/:id"
-                exact
-                component={ItemView}
-              />
-              <Route
-                path="/cart"
-                exact
-                render={(props) => <Cart {...props} shoppingCart={shoppingCart}/>}
-              />
-              <Route
-                path="/checkout"
-                exact
-                render={(props) => <Checkout {...props} shoppingCart={shoppingCart}/>}
-              />
-              </Switch>
-            </Grid>
-          <Grid item xs={1} sm={2} />
-        </Grid>
-      </BrowserRouter>
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid container direction="column">
+        <BrowserRouter>
+          <Grid item>
+            <Header />
+          </Grid>
+          <Grid item container>
+            <Grid item xs={1} sm={2} />
+              <Grid item xs={12} sm={8}>
+              <Switch>
+                <Route
+                  path="/"
+                  exact
+                  render={(props) => <StoreList {...props} addToCart={addToCart}/>}
+                />
+                <Route
+                  path="/product/:id"
+                  exact
+                  component={ItemView}
+                />
+                <Route
+                  path="/cart"
+                  exact
+                  render={(props) => <Cart {...props} shoppingCart={shoppingCart}/>}
+                />
+                <Route
+                  path="/checkout"
+                  exact
+                  render={(props) => <Checkout {...props} shoppingCart={shoppingCart}/>}
+                />
+                </Switch>
+              </Grid>
+            <Grid item xs={1} sm={2} />
+          </Grid>
+        </BrowserRouter>
+      </Grid>
+    </ThemeProvider>
   )
 }
 
