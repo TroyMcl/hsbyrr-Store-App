dfd99f476dec8aaeac11ebc4a0a6ffebd1525c0d
Troy <troyjmcl85@gmail.com>
2020-12-14 11:56:21 -0700

    added visual search bar to header, styled search bar, cart and store buttons, added gradient background to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,91 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton'
 import { AppBar, Toolbar, Button } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { makeStyles } from '@material-ui/styles';
+import SearchIcon from '@material-ui/icons/Search'
+import { fade, makeStyles } from '@material-ui/core/styles';
+import theme from '../theme';
 
 const useStyles = makeStyles({
   font: {
     color: '#ffffff',
-    textDecoration: 'none'
+    margin: '10px',
+    fontSize: '18px'
   },
-
   appBar: {
     display: 'flex',
-    justifyContent: 'space-between'
-  }
+    justifyContent: 'space-between',
+    paddingLeft: '100px',
+    paddingRight: '100px',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 })
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar position="static" style={{marginBottom: '20px'}}>
+    <AppBar position="static" style={{ marginBottom: '20px', background: 'linear-gradient(to right bottom, #9c27b0, #d05ce3)' }}>
       <Toolbar className={classes.appBar}>
-        <Link to="/" className="item">
-        <Button className={classes.font}>Store</Button>
-        </Link>
-        <Link to="/cart" className="item">
-          <IconButton edge="start" className={classes.font}>
-            <ShoppingCartIcon />
-          </IconButton>
-        </Link>
+        <div className={classes.search}>
+          <div className={classes.searchIcon} >
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search.."
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}
+          />
+        </div>
+        <div>
+          <Link to="/" className="item" style={{ textDecoration: 'none' }}>
+            <Button className={classes.font}>Store</Button>
+          </Link>
+          <Link to="/cart" className="item">
+            <IconButton edge="start" className={classes.font}>
+              <ShoppingCartIcon />
+            </IconButton>
+          </Link>
+        </div>
       </Toolbar>
     </AppBar>
   )
