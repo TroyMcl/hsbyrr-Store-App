49de53ca6f85f624994436940e0a010f2243221c
Troy <troyjmcl85@gmail.com>
2020-12-16 11:04:49 -0700

    Added 3 ratings components for effictiveness, delivery and quality

--- src/components/products/ItemPage/Review.js
+++ src/components/products/ItemPage/Review.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Box } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { Check } from '@material-ui/icons';
+import SentimentVeryDissatisfiedRoundedIcon from '@material-ui/icons/SentimentVeryDissatisfiedRounded';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+
+const customRatingIcons = {
+  1: {
+    icon: < SentimentVeryDissatisfiedRoundedIcon />,
+    label: 'Very Dissatisfied'
+  },
+  2: {
+    icon: < SentimentVeryDissatisfiedIcon />,
+    label: 'Dissatisfied'
+  },
+  3: {
+    icon: < SentimentSatisfiedIcon />,
+    label: 'Neutral'
+  },
+  4: {
+    icon: < SentimentSatisfiedAltIcon />,
+    label: 'Satisfied'
+  },
+  5: {
+    icon: < SentimentVerySatisfiedIcon />,
+    label: 'Very Satisfied'
+  }
+}
+
+function IconContainer(props) {
+  const {value, ...other} = props;
+  return <span {...other}>{customRatingIcons[value].icon}</span>
+}
 
 const Review = (props) => {
   const {userName, starRating, verifiedPurchaser, reviewDate, review, images, recommend, effictiveness, fastDelivery, quality } = props.review;
@@ -11,7 +44,33 @@ const Review = (props) => {
     <Grid container>
       <Grid item xs={12} sm={3} md={3} lg={3}>
         <Typography>{userName}</Typography>
-
+        <Box>
+          <Typography component="legend">Effictiveness</Typography>
+          <Rating
+            name="effictivenessRating"
+            defaultValue={effictiveness}
+            getLabelText={(value) => customRatingIcons[value].label}
+            IconContainerComponent={IconContainer}
+          />
+        </Box>
+        <Box>
+          <Typography component="legend">Fast Delivery</Typography>
+          <Rating
+            name="delivery"
+            defaultValue={fastDelivery}
+            getLabelText={(value) => customRatingIcons[value].label}
+            IconContainerComponent={IconContainer}
+          />
+        </Box>
+        <Box>
+          <Typography component="legend">Quality</Typography>
+          <Rating
+            name="qualityRating"
+            defaultValue={quality}
+            getLabelText={(value) => customRatingIcons[value].label}
+            IconContainerComponent={IconContainer}
+          />
+        </Box>
       </Grid>
       <Grid item xs={12} sm={9} md={9} lg={9}>
       <Rating name="totalRating" value={starRating} readOnly />
