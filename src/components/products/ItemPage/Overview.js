222b7e25a36f851c19601f75105169cb581cb88e
Troy <troyjmcl85@gmail.com>
2020-12-17 13:00:15 -0700

    Added styling to Oveview, set fontsize, padding and added lower border between sections

--- src/components/products/ItemPage/Overview.js
+++ src/components/products/ItemPage/Overview.js
@@ -1,33 +1,86 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  section: {
+    padding: 5,
+    marginBottom: 15,
+    marginTop: 15,
+  },
+  pargraphStyles: {
+    borderBottom: '1px solid #d3d3d3',
+    fontWeight: 'lighter',
+    paddingBottom: 15,
+    fontSize: 14
+  },
+  headerStyles: {
+    fontSize: 15,
+    fontWeight: 'lighter'
+  }
+})
+
 
 const Overview = (props) => {
   const { description, materials, recUsage } = props;
-
+  const classes = useStyles();
   return (
     <div>
-      <Grid container>
+      <Grid container className={classes.section}>
         <Grid item xs={12} sm={3} md={3} lg={3} >
-          <Typography>Description</Typography>
+          <Typography
+            className={classes.headerStyles}
+            variant="h4"
+          >
+            Description
+          </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9} lg={9} >
-          <Typography>{description}</Typography>
+          <Typography
+            className={classes.pargraphStyles}
+            variant="body2"
+          >
+            {description}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid container>
-      <Grid item xs={12} sm={3} md={3} lg={3} >
-          <Typography>Materials</Typography>
+      <Grid container className={classes.section}>
+        <Grid item xs={12} sm={3} md={3} lg={3} >
+          <Typography
+            className={classes.headerStyles}
+            variant="h4"
+          >
+            Materials
+          </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9} lg={9} >
-          <Typography>{materials}</Typography>
+          <Typography
+            className={classes.pargraphStyles}
+            variant="body2"
+          >
+            {materials}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid container>
-      <Grid item xs={12} sm={3} md={3} lg={3} >
-          <Typography>Recommended Usage</Typography>
+      <Grid container className={classes.section}>
+        <Grid item xs={12} sm={3} md={3} lg={3} >
+          <Typography
+            className={classes.headerStyles}
+            variant="h4"
+          >
+            Recommended Usage
+          </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9} lg={9} >
-          <Typography>{recUsage}</Typography>
+          <Typography
+            className={classes.pargraphStyles}
+            variant="body2"
+          >
+            {recUsage}
+          </Typography>
         </Grid>
       </Grid>
     </div>
