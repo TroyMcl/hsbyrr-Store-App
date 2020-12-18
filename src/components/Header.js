87e8258d51ff40633dc4b76f4fb0fc4bab8b3503
Troy <troyjmcl85@gmail.com>
2020-12-17 17:03:36 -0700

    Added badge for shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton'
-import { AppBar, Toolbar, Button } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Grid,
+  Box,
+  Badge
+} from '@material-ui/core';
 import InputBase from '@material-ui/core/InputBase';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import SearchIcon from '@material-ui/icons/Search'
 import { fade, makeStyles } from '@material-ui/core/styles';
 import theme from '../theme';
+import { ShoppingCartContext } from './shoppingCartContext';
 
 const useStyles = makeStyles({
   font: {
     color: '#ffffff',
     margin: '10px',
     fontSize: '18px'
-  },
-  appBar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    // paddingLeft: '100px',
-    paddingRight: '100px',
   },
   search: {
     position: 'relative',
@@ -61,32 +63,54 @@ const useStyles = makeStyles({
 })
 
 const Header = () => {
+  const [shoppingCart, useShoppingCart, addToCart, adjustQty] = useContext(ShoppingCartContext);
   const classes = useStyles();
   return (
     <AppBar position="static" style={{ marginBottom: '20px', background: 'linear-gradient(to right bottom, #9c27b0, #d05ce3)' }}>
-      <Toolbar className={classes.appBar}>
-        <div className={classes.search}>
-          <div className={classes.searchIcon} >
-            <SearchIcon />
-          </div>
-          <InputBase
-            placeholder="Search.."
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput
-            }}
-          />
-        </div>
-        <div>
-          <Link to="/" className="item" style={{ textDecoration: 'none' }}>
-            <Button className={classes.font}>Store</Button>
-          </Link>
-          <Link to="/cart" className="item">
-            <IconButton edge="start" className={classes.font}>
-              <ShoppingCartIcon />
-            </IconButton>
-          </Link>
-        </div>
+      <Toolbar>
+        <Grid container>
+          <Grid item xs={1} sm={1} md={1} />
+          <Grid item xs={12} sm={5} md={5}>
+            <Box
+              marginTop='20px'
+              display="flex"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <div className={classes.search}>
+                <div className={classes.searchIcon} >
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search.."
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                />
+              </div>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={5} md={5}>
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              <Link to="/" className="item" style={{ textDecoration: 'none' }}>
+                <Button className={classes.font}>Store</Button>
+              </Link>
+              <Link to="/cart" className="item">
+                <IconButton edge="start" className={classes.font}>
+                  <Badge badgeContent={shoppingCart.length} color='secondary' >
+                    <ShoppingCartIcon />
+                  </Badge>
+                </IconButton>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={1} sm={1} md={1} />
+        </Grid>
       </Toolbar>
     </AppBar>
   )
